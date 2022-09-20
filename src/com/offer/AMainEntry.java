33cfc423b30a71a5486e02a1bb8716baba55deca
kangwen <1397796710@qq.com>
2022-09-20 14:50:04 +0800

    Update Path1_2.java.

--- src/com/offer/AMainEntry.java
+++ src/com/offer/AMainEntry.java
@@ -5,8 +5,8 @@ package com.offer;
 
 public class AMainEntry {
     public static void main(String[] args){
-        new Path1_1();
-//        new Path1_2();
+//        new Path1_1();
+        new Path1_2();
         
 
     }
