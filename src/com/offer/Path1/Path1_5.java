2e334b7677d4d5ca53e580760cd0d558fe5b0e25
kangwen <1397796710@qq.com>
2022-09-23 17:06:23 +0800

    单词长度的最大乘积.

--- src/com/offer/Path1/Path1_5.java
+++ src/com/offer/Path1/Path1_5.java
@@ -12,7 +12,35 @@ import com.offer.APathBase;
 public class Path1_5 extends APathBase {
     @Override
     public void Start() {
-
+        String[] words = { "abcw", "foo", "bar", "fxyz", "abcdef" };
+        int result = maxProduct(words);
+        print(result);
     }
 
+    private int maxProduct(String[] words){
+        boolean[][] flags = new boolean[words.length][26];
+        for (int i = 0; i < words.length; i++) {
+            for (char c : words[i].toCharArray()){
+                flags[i][c - 'a'] = true;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                int k = 0;
+                for (; k < 26; k++) {
+                    if(flags[i][k] && flags[j][k]){
+                        break;
+                    }
+                }
+
+                if(k == 26){
+                    int prod = words[i].length() * words[j].length();
+                    result = Math.max(result, prod);
+                }
+            }
+        }
+        return result;
+    }
 }
