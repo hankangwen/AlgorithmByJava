0dd12fc3b8ad0e8d0bebc72371878fade5fb7e38
kangwen <1397796710@qq.com>
2022-09-26 11:16:28 +0800

    数组中和为0的3个数字.

--- src/com/offer/Path2/Path2_7.java
+++ src/com/offer/Path2/Path2_7.java
@@ -2,6 +2,10 @@ package com.offer.Path2;
 
 import com.offer.APathBase;
 
+/* 数组中和为0的3个数字
+* 输入一个数组，如何找出数组中所有和为0的3个数字的三元组？需要注意的是，返回值中不得包含重复的三元组。
+* 例如，在数组[-1，0，1，2，-1，-4]中有两个三元组的和为0，它们分别是[-1，0，1]和[-1，-1，2]。
+* */
 public class Path2_7 extends APathBase {
     @Override
     public void Start() {
