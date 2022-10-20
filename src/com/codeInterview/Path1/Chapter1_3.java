fb4ebb8ae187760812146b330ea26ee2bd32fe17
hankangwen <13538898378@163.com>
2022-10-20 21:10:54 +0800

    Update Chapter1_3.java

--- src/com/codeInterview/Path1/Chapter1_3.java
+++ src/com/codeInterview/Path1/Chapter1_3.java
@@ -2,8 +2,10 @@ package com.codeInterview.Path1;
 
 import com.offer.APathBase;
 
-/*
-【题目】
+/*如何仅用递归函数和栈操作逆序一个栈
+【题目】一个栈依次压入1、2、3、4、5，那么从栈顶到栈底分别为5、4、3、2、1。
+将这个栈转置后，从栈顶到栈底为1、2、3、4、5，也就是实现栈中元素的逆序，
+但是只能用递归函数来实现，不能用其他数据结构。
 【要求】
 */
 public class Chapter1_3 extends APathBase {
