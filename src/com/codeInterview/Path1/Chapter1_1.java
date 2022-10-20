ff830bb1f7a371531500f9d5d2dcf0d241d5a3dc
hankangwen <13538898378@163.com>
2022-10-20 14:09:39 +0800

    设计一个有getMin功能的栈.

--- src/com/codeInterview/Path1/Chapter1_1.java
+++ src/com/codeInterview/Path1/Chapter1_1.java
@@ -3,7 +3,7 @@ package com.codeInterview.Path1;
 import com.offer.APathBase;
 import java.util.Stack;
 
-/*
+/*设计一个具有getMin功能的栈
 【题目】实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
 【要求】1．pop、push、getMin操作的时间复杂度都是O(1)。
 2．设计的栈类型可以使用现成的栈结构。
@@ -13,14 +13,51 @@ public class Chapter1_1 extends APathBase {
     @Override
     public void Start() {
         int[] arrays = new int[]{3,4,5,1,2,1};
-        
-        MyStack1 myStack1 = new MyStack1();
+
+        MyStack2 myStack1 = new MyStack2();
         for (int array : arrays) {
             myStack1.push(array);
         }
         for (int i = 0; i < arrays.length; i++) {
             print(myStack1.pop());
         }
+    }
+}
+
+class MyStack2{
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack2(){
+        this.stackData = new Stack<Integer>();
+        this.stackMin = new Stack<Integer>();
+    }
+
+    public void push(int newNum){
+        if(this.stackMin.isEmpty()){
+            this.stackMin.push(newNum);
+        } else if (newNum < this.getMin()) {
+            this.stackMin.push(newNum);
+        } else {
+            int newMin = this.stackMin.peek();
+            this.stackMin.push(newMin);
+        }
+        this.stackData.push(newNum);
+    }
+
+    public int pop(){
+        if(this.stackData.isEmpty()){
+            throw new RuntimeException("Your stack is empty.");
+        }
+        this.stackMin.pop();
+        return this.stackData.pop();
+    }
+
+    public int getMin(){
+        if(this.stackMin.isEmpty()){
+            throw new RuntimeException("Your stack is empty.");
+        }
+        return this.stackMin.peek();
     }
 }
 
